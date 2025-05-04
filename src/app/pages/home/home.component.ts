0c4942823d2c674b120eec136fa5594303c74534
Binayak Purohit <93867057+binayak-tech@users.noreply.github.com>
2025-05-04 14:57:10 +0530

    removed ssr

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -41,9 +41,9 @@ import { title } from 'process';
 export class HomeComponent implements OnInit, OnDestroy {
 
   heroItems = [
-    {image: 'assets/images/hero/hero-bg-1.jpg', title: 'Speak Global Languages. Open Global Doors'},
+    {image: 'assets/images/hero/hero-bg-1.jpg', title: 'Speak Fluently, Lead Confidently'},
     {image: 'assets/images/hero/hero-bg-2.jpg', title: 'Ace your IELTS with Expert Guidance'},
-    {image: 'assets/images/hero/hero-bg-3.jpg', title: 'Speak Fluently, Lead Confidently'},
+    {image: 'assets/images/hero/hero-bg-3.jpg', title: 'Speak Global Languages. Open Global Doors'},
     {image: 'assets/images/hero/hero-bg-4.jpg', title: 'Fluent Learning. Right from your home.'}
   ]
   
